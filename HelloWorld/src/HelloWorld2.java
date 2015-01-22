89f8a9f960d4a0067b67a3ec85d31cf76e49d3ef
weipanchang <weipanchang@hotmail.com>
2015-01-22 13:24:14 -0800

    First Commit

---
+++ HelloWorld/src/HelloWorld2.java
@@ -0,0 +1,13 @@
+public class HelloWorld2
+{
+   public static void main(String[] args) 
+   {
+      String name = "Bob"; 
+      String greeting = "Hello";
+      System.out.println(greeting + ", " + name);
+      name = "ChingFen";
+      System.out.println(greeting + ", " + name + "!");
+      greeting = "Goodbye";
+      System.out.println(greeting + ", " + name + "!");
+   }
+}
